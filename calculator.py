7f86cf8ae9969baa25a1ff8bccd7be31014ddab3
bereket tesfaye <berekettesfaye7909@gmail.com>
2024-02-10 11:40:47 +0100

    this is my third commit and i have add subtract method

---
+++ calculator.py
@@ -0,0 +1,8 @@
+class calculator:
+    def add(self,num1,num2):
+        num1_num2=num1 + num2
+        return f"{num1} + {num2} = {num1_num2}"
+    def subtract(self,num1,num2):
+        num1_num2=num1-num2
+        return f"{num1} - {num2} = {num1_num2}"
+    
