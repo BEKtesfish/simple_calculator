f1064ccc2e3c2a7b994009d298ebd94dd2487532
bereket tesfaye <berekettesfaye7909@gmail.com>
2024-02-10 11:52:08 +0100

    add some description about the calculator and a method for dividing two numbers

--- calculator.py
+++ calculator.py
@@ -1,11 +1,20 @@
+# this is  a simple calculator that does addition, multiplication,
+#subtraction, division. this calculator is only capable of calculating two numbers
 class calculator:
     def add(self,num1,num2):
         num1_num2=num1 + num2
         return f"{num1} + {num2} = {num1_num2}"
+    
     def subtract(self,num1,num2):
         num1_num2=num1-num2
         return f"{num1} - {num2} = {num1_num2}"
+    
     def multiply(self,num1,num2):
         num1_num2=num1 * num2
         return f"{num1} * {num2} = {num1_num2}"
+    
+    def divide(self,num1,num2):
+        assert num2!=0, f"invalid in put --> {num2} "
+        num1_num2=num1 / num2
+        return f"{num1} / {num2} = {num1_num2}"
     
